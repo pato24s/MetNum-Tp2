4f3cd305426313c0d9c76e562668e35cc1284b28
Juan Gonzalez <gonzalezjuan.ab@gmail.com>
2016-11-30 12:40:08 -0300

    script para dibujar entradas en progreso

---
+++ src/dibujar.py
@@ -0,0 +1,45 @@
+from Tkinter import *
+import math
+import os
+from PIL import Image, ImageDraw
+import PIL.ImageOps 
+import io
+
+def guardar(event):
+    ps = lienzo.postscript(colormode='monochrome')
+    img = Image.open(io.BytesIO(ps.encode('utf-8')))
+    img.save('test.bmp')
+
+    im = Image.open("test.bmp")
+    inverted_image = PIL.ImageOps.invert(im)
+    inverted_image.save('invertido.bmp')
+    
+    #img = img.resize((28, 28), Image.ANTIALIAS)
+    #img.save('nuevaMedida.bmp')
+    #img = Image.open('nuevaMedida.bmp')
+    pix = inverted_image.load()
+    img2 = []
+    for i in range(im.size[0]):
+        img2_fila = []
+        for j in range(im.size[1]):
+            pixel = pix[i,j]
+            pixel_cromatico = 0.2989 * pixel[0] + 0.5870 * pixel[1] + 0.1140 * pixel[2] 
+            img2.append(int(pixel_cromatico))
+
+    #print(img2)
+
+
+
+    root.quit()
+
+def dibujar(event):
+    lienzo.create_oval(event.x, event.y, event.x+1, event.y+1,width = 20)
+
+
+root = Tk()
+lienzo = Canvas(root, width=300, height=300, background= 'white')
+lienzo.bind("<B1-Motion>", dibujar)
+lienzo.bind("<Button-3>", guardar)
+lienzo.pack()
+
+root.mainloop()
