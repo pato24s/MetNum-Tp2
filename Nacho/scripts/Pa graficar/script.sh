acd142c0c0f12038fab80fc7395fbcc4411ee561
Juan Alberto Gonzalez Benitez <jagonzalez@ws4.labo4>
2016-12-02 14:35:05 -0300

    limpiezo

--- Nacho/scripts/Pa graficar/script.sh
+++
@@ -1,7 +0,0 @@
-make clean 
-make
-for(( i = 128; i < 1700; i=i+128 )); do
-     	echo "corriendo filtro para una matriz de $i x $i"
-	  printf '%i   ' $(($i*$i)) >> SEPIAFATFAST
-	  ./build/tp2 sepia -i asm ./img/lena.${i}x${i}.bmp -t 100 >>SEPIAFATFAST
-	 done
